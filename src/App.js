7d0440db6ebad14ebe97df30375848a2975ead3e
hawrot <mhawro200@caledonian.ac.uk>
2020-05-07 23:35:50 +0100

    Import the component into the main app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1> Hello from React</h1>
         <p>This works</p>
+        <Person/>
     </div>
+
   );
 }
 
