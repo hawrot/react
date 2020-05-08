50fe4d81e4b8b1b59d14c157f3ee2f009f2c2b9e
hawrot <mhawro200@caledonian.ac.uk>
2020-05-08 14:54:15 +0100

    Passing data using arrow function

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const switchNameHandler = (newName) =>{
     <div className="App">
       <h1> Hello from React</h1>
         <p>This works</p>
-        <button onClick={switchNameHandler.bind(this, 'Mateusz H')}>Switch name</button>
+        <button onClick={() =>switchNameHandler('Matt H')}>Switch name</button>
         <Person name={personsState.persons[0].name}/>
       <Person name={personsState.persons[1].name}>My hobbies: None yet</Person>
         <Person click={this.switchNameHandler} name={personsState.persons[2].name} />
