b9b63695058ae0b3dfee3cb0e03d6b15a2acbdb5
hawrot <mhawro200@caledonian.ac.uk>
2020-05-08 23:32:30 +0100

    Add styleroot for main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
+import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
     state = {
@@ -79,12 +79,14 @@ class App extends Component {
         }
 
         return (
+            <StyleRoot>
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
                 <p className={classes.join(' ')}>This is really working!</p>
                 <button style={style} onClick={this.togglePersonHandler}>{this.state.persons ? 'Show persons': 'Hide Persons'}</button>
                 {persons}
             </div>
+            </StyleRoot>
         );
 
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
