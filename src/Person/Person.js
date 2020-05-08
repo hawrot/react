eea844c501ccdcd2552da48f9fc744ed64af75ce
hawrot <mhawro200@caledonian.ac.uk>
2020-05-08 18:14:32 +0100

    Change the structure of App.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,17 +1,13 @@
-import React from "react";
-import './Person.css';
+import React from 'react';
 
-const person = (props) => {
-    return(
+const person = ( props ) => {
+    return (
         <div>
-            <p onClick={props.click}>I am a {props.name}, and I am {Math.floor(Math.random()*30)}</p>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
-
-
     )
-
 };
 
 export default person;
