f4b00215713a81375bae87b2ca38ca4b34d81475
hawrot <mhawro200@caledonian.ac.uk>
2020-05-08 13:15:26 +0100

    Using "useState" for onClick handler

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
+const App = props => {
+
+   const [ personsState, setPersonsState ] = useState({
+    persons: [
+        {name: 'Matt'},
+        {name: 'Mark'},
+        {name: 'Ellie'}
+    ]
+});
+
+const switchNameHandler = () =>{
+    setPersonsState({
+        persons: [
+            {name: 'Mateusz'},
+            {name: 'Tina'},
+            {name: 'Ellie'}
+        ]
+    })
+}
+
   return (
     <div className="App">
       <h1> Hello from React</h1>
         <p>This works</p>
-        <button>Switch name</button>
-        <Person name="Andrew"/>
-      <Person name="Garry">My hobbies: None yet</Person>
-        <Person name="Joe"/>
+        <button onClick={switchNameHandler}>Switch name</button>
+        <Person name={personsState.persons[0].name}/>
+      <Person name={personsState.persons[1].name}>My hobbies: None yet</Person>
+        <Person name={personsState.persons[2].name}/>
     </div>
   );
 }
 
 export default App;
+
+
