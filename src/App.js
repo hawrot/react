c70804763dfd1cf95fb7db2282ebd96c420ccfe7
hawrot <mhawro200@caledonian.ac.uk>
2020-05-08 23:06:18 +0100

    Create classes with styles

--- src/App.js
+++ src/App.js
@@ -61,6 +61,8 @@ class App extends Component {
             );
             style.backgroundColor = 'red';
         }
+        let classes = ['red', 'bold'].join(' ');
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
