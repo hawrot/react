ecf0286e78365dde332aeba70259d329b42e5dee
hawrot <mhawro200@caledonian.ac.uk>
2020-05-10 23:32:52 +0100

    Passing Unknown props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Aux from '../../../hoc/Aux';
 import classes from './Person.css';
+import withClass from "../../../hoc/WithClass";
 
 
 class Person extends Component{
@@ -18,4 +19,4 @@ class Person extends Component{
 
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
