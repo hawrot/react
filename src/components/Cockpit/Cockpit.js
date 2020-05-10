221084c23aad831c0ffdd5ecd8c4d7754449afa6
hawrot <mhawro200@caledonian.ac.uk>
2020-05-10 18:32:20 +0100

    Move cockpit view into cockpit component

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const cockpit = (props) => {
+    return (
+        <div>
+            <h1>Hi, I'm a React App</h1>
+             <p className={assignedClasses.join(' ')}>This is really working!</p>
+             <button className={btnClass}  onClick={this.togglePersonHandler}>{this.state.persons ? 'Show persons': 'Hide Persons'}</button>
+        </div>
+    );
+};
+
+
+export default cockpit;
