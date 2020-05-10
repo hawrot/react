4925e21c18b7cb9224f28bc835efc5b55faabaa3
hawrot <mhawro200@caledonian.ac.uk>
2020-05-10 23:40:49 +0100

    Setting state correctly on change counter

--- src/containers/App.js
+++ src/containers/App.js
@@ -22,7 +22,8 @@ class App extends Component {
             {id: '23r2df', name: 'Stephanie', age: 26}
         ],
         otherState: 'some other value',
-        showPersons: false
+        showPersons: false,
+        changeCounter: 0
     }
 
     static getDerivedStateFromProps(props, state){
@@ -56,9 +57,13 @@ class App extends Component {
         const persons = [...this.state.persons];
         persons[personIndex] = person;
 
-        this.setState({
-            persons: persons
-        })
+        this.setState((prevState, props) =>{
+            return{
+                persons: persons,
+                changeCounter: prevState.changeCounter + 1
+            }
+        }
+        )
     }
 
     render() {
@@ -72,7 +77,6 @@ class App extends Component {
                     changed={this.nameChangedHandler}
                 />
         }
-
 
         return (
 
