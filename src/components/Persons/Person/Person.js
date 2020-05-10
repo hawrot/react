bc98d575f02b5c8770db75d986134be76f1c5e18
hawrot <mhawro200@caledonian.ac.uk>
2020-05-10 23:52:56 +0100

    Using refs for focusing element

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,6 +6,11 @@ import PropTypes from 'prop-types';
 
 
 class Person extends Component{
+
+    componentDidMount() {
+        this.inputElement.focus();
+    }
+
     render() {
         console.log('Person.js rendering');
 
@@ -13,7 +18,7 @@ class Person extends Component{
         <React.Fragment>
             <p  onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p key="i2">{this.props.children}</p>
-                <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
+                <input ref={(inputEl) => {this.inputElement = inputEl}} key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
         </React.Fragment>
         )
     }
@@ -23,7 +28,8 @@ class Person extends Component{
 Person.propTypes = {
     click : PropTypes.func,
     name :PropTypes.string,
-    age : PropTypes.number
+    age : PropTypes.number,
+    change : PropTypes.func
 };
 
 export default withClass(Person, classes.Person);
