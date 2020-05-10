0e2443501b71c3b0fb4e96916398a943ff862950
hawrot <mhawro200@caledonian.ac.uk>
2020-05-10 18:42:01 +0100

    Move logic into Cockpit component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import classes from './App.css'
 import Persons  from '../components/Persons/Persons'
+import Cockpit from '../components/Cockpit/Cockpit';
 
 
 
@@ -44,34 +45,25 @@ class App extends Component {
     }
 
     render () {
-        let btnClass = '';
         let persons = null;
         if(this.state.showPersons){
-            persons = (
-                <div>
-                    <Persons
+            persons =
+                <Persons
                         persons={this.state.persons}
                         clicked={this.deletePersonHandler}
                         changed={this.nameChangedHandler}
                     />
-                </div>
-            );
-        btnClass = classes.Red;
         }
-        let assignedClasses = [];
-        if (this.state.persons.length <= 2){
-            assignedClasses.push(classes.red);
-        }
-        if (this.state.persons.length >= 1){
-            assignedClasses.push(classes.bold);
-        }
+
 
         return (
 
             <div className={classes.App}>
-                <h1>Hi, I'm a React App</h1>
-                <p className={assignedClasses.join(' ')}>This is really working!</p>
-                <button className={btnClass}  onClick={this.togglePersonHandler}>{this.state.persons ? 'Show persons': 'Hide Persons'}</button>
+                <Cockpit
+                    showPersons={this.state.showPersons}
+                    persons={this.state.persons}
+                    clicked={this.togglePersonHandler}
+                />
                 {persons}
             </div>
 
