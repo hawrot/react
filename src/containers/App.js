eb8540c2b26d4ba6ab2ac0fd649b93c225144c3f
hawrot <mhawro200@caledonian.ac.uk>
2020-05-10 23:21:14 +0100

    Replacing div with WithClasses

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import classes from './App.css'
 import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 
 class App extends Component {
@@ -74,7 +75,7 @@ class App extends Component {
 
         return (
 
-            <div className={classes.App}>
+            <WithClass classes={classes.App}>
                 <Cockpit
                     title={this.props.appTitle}
                     showPersons={this.state.showPersons}
@@ -82,7 +83,7 @@ class App extends Component {
                     clicked={this.togglePersonHandler}
                 />
                 {persons}
-            </div>
+            </WithClass>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
     }
