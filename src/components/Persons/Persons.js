67c6ebe95ca7bb187be2d2fedf5bd36a22a092e6
hawrot <mhawro200@caledonian.ac.uk>
2020-05-10 20:51:43 +0100

    Using componentDidMount and getDerivedStateFromProps

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Person from "./Person/Person";
 
-const persons = (props) => (
-    props.persons.map((p, index) => {
+const persons = (props) => {
+    console.log('Persons.js redner');
+    return props.persons.map((p, index) => {
         return <Person click={ () => props.clicked(index)} name={p.name} key={p.id} changed={(event) => props.changed(event, p.id)}/>
     }
-));
-
+);
+}
 export default persons;
--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,13 @@ import Cockpit from '../components/Cockpit/Cockpit';
 
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        console.log('[App.js] constructor');
+
+    }
+
     state = {
         persons: [
             {id: 'sdfsdf', name: 'Max', age: 28},
@@ -14,6 +21,15 @@ class App extends Component {
         ],
         otherState: 'some other value',
         showPersons: false
+    }
+
+    static getDerivedStateFromProps(props, state){
+        console.log('[App.js] getGerivedState' + props);
+        return state;
+    }
+
+    componentDidMount() {
+        console.log('App.js component did mount')
     }
 
     deletePersonHandler = (personIndex) => {
@@ -44,6 +60,7 @@ class App extends Component {
     }
 
     render() {
+        console.log('App.js render');
         let persons = null;
         if (this.state.showPersons) {
             persons =
@@ -59,6 +76,7 @@ class App extends Component {
 
             <div className={classes.App}>
                 <Cockpit
+                    title={this.props.appTitle}
                     showPersons={this.state.showPersons}
                     persons={this.state.persons}
                     clicked={this.togglePersonHandler}
