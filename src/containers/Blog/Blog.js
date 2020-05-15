7109b5c8da5e5bd3e4b46a0198f2f898430f8cad
hawrot <mhawro200@caledonian.ac.uk>
2020-05-16 00:12:00 +0100

    Using Switch to render only one component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import {Route, NavLink} from 'react-router-dom';
+import React, {Component} from 'react';
+import {Route, NavLink, Switch} from 'react-router-dom';
 
 
 import './Blog.css';
@@ -10,11 +10,7 @@ import FullPost from "./FullPost/FullPost";
 class Blog extends Component {
 
 
-
-
-
-
-    render () {
+    render() {
 
 
         return (
@@ -30,13 +26,16 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-               {/* <Route path="/" exact render={()=> <Posts/>}/>
+                {/* <Route path="/" exact render={()=> <Posts/>}/>
                 <Route path="/" exact render={()=> <FullPost/>}/>
                 <Route path="/new-post" exact render={()=> <NewPost/>}/>*/}
 
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post"  component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                <Switch>
+                    <Route path="/" exact component={Posts}/>
+                    <Route path="/new-post" component={NewPost}/>
+                    <Route path="/:id" exact component={FullPost}/>
+                </Switch>
+
             </div>
         );
     }
