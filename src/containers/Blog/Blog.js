764e48bf49a1f02ff1cbfe8a24d1cba0d920c04b
hawrot <mhawro200@caledonian.ac.uk>
2020-05-16 13:39:58 +0100

    Fix the problem with no acces to axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,45 +1,43 @@
-import React, {Component} from 'react';
-import {Route, NavLink, Switch} from 'react-router-dom';
-
+import React, { Component } from 'react';
+// import axios from 'axios';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from "./NewPost/NewPost";
-
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
-
-
-    render() {
-
-
+    render () {
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink to="/" exact>Posts</NavLink></li>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
-                                hash: '#submit'
+                                hash: '#submit',
+                                search: '?quick-submit=true'
                             }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
-                {/* <Route path="/" exact render={()=> <Posts/>}/>
-                <Route path="/" exact render={()=> <FullPost/>}/>
-                <Route path="/new-post" exact render={()=> <NewPost/>}/>*/}
-
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-
-                    <Route path="/new-post" component={NewPost}/>
-                    <Route path="/"  component={Posts}/>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />
                 </Switch>
-
             </div>
         );
     }
 }
 
 export default Blog;
-
