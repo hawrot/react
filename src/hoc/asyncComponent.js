c5aa6300f1f0573e64bba56005e81ada55d1a04c
hawrot <mhawro200@caledonian.ac.uk>
2020-05-16 16:59:43 +0100

    return component in asyncComp

--- src/hoc/asyncComponent.js
+++ src/hoc/asyncComponent.js
@@ -1,21 +1,22 @@
 import React, {Component} from 'react';
 
-const asyncComponent = (importComponent) =>{
-    return class extends Component{
+const asyncComponent = (importComponent) => {
+    return class extends Component {
         state = {
             component: null
         }
 
         componentDidMount() {
             importComponent()
-                .then(cmp =>{
+                .then(cmp => {
                     this.setState({component: cmp.default});
                 })
         }
-        render() {
-            return super.render();
-        }
 
+        render() {
+            const C = this.state.component;
+            return C ? <C {...this.props}/> : null;
+        }
     }
 }
 
